48a0e0e5745c591e5f6ef0a41a224d3d14a50217
Kevin Jensen <jensenk28@yahoo.com>
2020-02-10 13:28:04 -0800

    Everyting working correctly now need to figure out how to clear completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import GroceryList from './components/TodoComponents/GroceryList';
+import TasksList from './components/TodoComponents/TasksList';
 import ListForm from './components/TodoComponents/ListForm';
 import './ToDo.css';
 
@@ -104,8 +104,8 @@ class App extends React.Component {
           <h1>ToDo List</h1>
           <ListForm addNewItem={this.addNewItem} />
         </div>
-        <GroceryList
-          groceries={this.state.tasksList}
+        <TasksList
+          tasks={this.state.tasksList}
           toggleItem={this.toggleItem}
         />
       </div>
--- src/components/TodoComponents/TasksList.js
+++ src/components/TodoComponents/TasksList.js
@@ -5,8 +5,8 @@ import Item from './Item';
 const GroceryList = props => {
   // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
   return (
-    <div className="shopping-list">
-      {props.groceries.map(item => (
+    <div className="tasks-list">
+      {props.tasks.map(item => (
         <Item key={item.id} item={item} toggleItem={props.toggleItem} />
       ))}
       <button className="clear-btn" onClick={props.clearPurchased}>
