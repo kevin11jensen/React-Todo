847588bb6b62f30ed2b3c43e29a3563fbe16c946
Kevin Jensen <jensenk28@yahoo.com>
2020-02-11 15:16:09 -0800

    able to delete items

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-export default props => 
-<div style={{
-  textDecoration: props.todo.complete ? 'line-through' : ''
-}} onClick={props.toggleComplete}>{props.todo.text}</div>;
+export default props => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div 
+      style={{
+      textDecoration: props.todo.complete ? 'line-through' : ''
+      }} 
+      onClick={props.toggleComplete}
+    >
+      {props.todo.text}
+    </div>
+    <button onClick={props.onDelete}></button>
+  </div>
+);
+
   
   
   
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -8,7 +8,7 @@ import Todo from './Todo';
 export default class TodoList extends React.Component {
   state = {
     todos: [],
-    
+    todoToShow: 'all'
   };
 
   addTodo = todo => {
@@ -37,19 +37,51 @@ export default class TodoList extends React.Component {
     });
   };
 
+  updateTodoToShow = (s) => {
+    this.setState({
+      todoToShow: s
+    })
+  }
+
+  handleDeleteTodo = (id) => {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    })
+  }
+
+
+
   render() {
+
+    let todos = [];
+
+    if (this.state.todoToShow === 'all') {
+      todos = this.state.todos;
+    } 
+    else if (this.state.todoToShow === 'active') {
+      todos = this.state.todos.filter(todo => !todo.complete);
+    }
+    else if (this.state.todoToShow === 'complete') {
+      todos = this.state.todos.filter(todo => todo.complete);
+    }
+
     return (
       <div>
         <TodoForm onSubmit={this.addTodo} />
-        {this.state.todos.map(todo => (
+        {todos.map(todo => (
           <Todo 
             key={todo.id} 
             toggleComplete={() => this.toggleComplete(todo.id)} 
-            
+            onDelete={() => this.handleDeleteTodo(todo.id)}
             todo={todo} 
           />
           ))}
           <div>tasks remaining: {this.state.todos.filter(todo => !todo.complete).length}</div>
+          <div>
+          <button onClick = {() => this.updateTodoToShow('all')}>all</button>
+          <button onClick = {() => this.updateTodoToShow('active')}>active</button>
+          <button onClick = {() => this.updateTodoToShow('completed')}>complete</button>
+          </div>
       </div>
     )
   }
